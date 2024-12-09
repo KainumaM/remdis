f211274e8a08bd9c799383fa27ec113b47e1a353
Yuki Yamamoto <yuki.yamamoto0915@gmail.com>
2024-12-09 13:13:05 +0900

    同じショートカットキーでOBSでも録画できるように修正

--- modules/recording.py
+++ modules/recording.py
@@ -1,13 +1,26 @@
 import pyautogui
+import pygetwindow as gw
 
-
+# Alt + F9 キーを押して録画を開始
 def start():
-    # Alt + F9 キーを押して録画を開始
+    activate_obs_window()
     pyautogui.hotkey("alt", "f9")
     print("Recording started...")
 
-
+# Alt + F9 キーを押して録画を停止
 def stop():
-    # Alt + F9 キーを押して録画を停止
+    activate_obs_window()
     pyautogui.hotkey("alt", "f9")
     print("Recording stopped...")
+
+# （laptop用）OBS をアクティブにする
+def activate_obs_window():
+    obs_window = None
+    for window in gw.getAllTitles():
+        if "OBS" in window:
+            obs_window = gw.getWindowsWithTitle(window)[0]
+            break
+    if obs_window:
+        obs_window.activate()
+    else:
+        print("OBS のウィンドウが見つかりません。")
